599825af10d91af37c374bac15b6f3cb578c0b09
vacool6 <sameerkittur321@gmail.com>
2024-01-09 12:20:10 +0530

    feat/cart + coveo functionalities

--- src/app/home-page/banner/banner.component.ts
+++ src/app/home-page/banner/banner.component.ts
@@ -1,3 +1,4 @@
+// banner.component.ts
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -5,8 +6,21 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css'],
 })
-export class BannerComponent {
-  @Input() imgSource: string = '';
+export class BannerComponent implements OnInit {
+  @Input() imageSources: string[] = [];
+  currentIndex: number = 0;
+  previousIndex: number = this.imageSources.length - 1;
 
   constructor() {}
+
+  ngOnInit() {
+    this.startImageSlideShow();
+  }
+
+  startImageSlideShow() {
+    setInterval(() => {
+      this.previousIndex = this.previousIndex;
+      this.currentIndex = (this.currentIndex + 1) % this.imageSources.length;
+    }, 5000);
+  }
 }
--- src/app/search-page/result-list/result-list.component.ts
+++ src/app/search-page/result-list/result-list.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-import {
-  headlessPager,
-  headlessResultList,
-  headlessResultsPerPage,
-} from '../../coveo/controllers';
+import { headlessResultList } from '../../coveo/controllers';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-result-list',
@@ -12,8 +9,63 @@ import {
 })
 export class ResultListComponent {
   results: any[] = [];
+  selectedSize: string = '';
+  selectedColorInfo: any = {};
+
+  resetData() {
+    this.selectedSize = '';
+    this.selectedColorInfo = {};
+  }
+
+  constructor(private cartService: CartService) {}
 
   ngDoCheck() {
     this.results = headlessResultList.state.results;
+  }
+
+  jsonParser(data: any) {
+    return JSON.parse(data);
+  }
+
+  sizeSelector(size: string) {
+    this.selectedSize = size;
+  }
+
+  colorSelector(colorInfo: any) {
+    this.selectedColorInfo.swatch = colorInfo.swatch;
+    this.selectedColorInfo.code = colorInfo.code;
+    this.selectedColorInfo.label = colorInfo.label;
+  }
+  addToCart(item: any) {
+    if (!this.selectedSize) return alert('Select a size');
+
+    if (
+      !this.selectedColorInfo.swatch ||
+      !this.selectedColorInfo.code ||
+      !this.selectedColorInfo.label
+    )
+      return alert('Select a color');
+
+    const data = {
+      id: Math.random(),
+      title: item.title,
+      image: item.raw['ec_images'][0],
+      size: this.selectedSize,
+      colorInfo: this.selectedColorInfo,
+      quantity: 1,
+      price: 1000,
+    };
+
+    this.cartService.addItemToCart(data);
+
+    this.selectedSize = '';
+    this.selectedColorInfo = {};
+
+    alert('Item added to cart');
+  }
+
+  mouseLeaveEvent() {
+    this.selectedSize = '';
+    this.selectedColorInfo = {};
   }
 }
